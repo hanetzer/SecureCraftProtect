e6fd60f50a13fa8447be4e3b1d777aec6d99e6cd
Marty Plummer <ntzrmtthihu777@gmail.com>
2014-07-22 15:24:15 -0500

    beginning of Class-D armor

---
+++ src/main/java/securecraftprotect/common/item/ItemArmorClassD.java
@@ -0,0 +1,9 @@
+package securecraftprotect.common.item;
+
+import net.minecraft.item.ItemArmor;
+
+public class ItemArmorClassD extends ItemArmor {
+    public ItemArmorClassD(ArmorMaterial material, int type, int index) {
+        super(material, type, index);
+    }
+}
--- src/main/java/securecraftprotect/init/SCPItems.java
+++ src/main/java/securecraftprotect/init/SCPItems.java
@@ -5,4 +5,5 @@ import net.minecraft.item.Item;
 public class SCPItems {
     public static Item.ToolMaterial toolMaterial1023;
     public static Item scp1023ARC;
+    public static Item gasMask;
 }
