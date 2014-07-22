b5deef9d1522ff67a47360aebec44ff814345398
Marty Plummer <ntzrmtthihu777@gmail.com>
2014-07-22 07:15:35 -0500

    SCP-1023ARC added

---
+++ src/main/java/securecraftprotect/init/SCPItems.java
@@ -0,0 +1,8 @@
+package securecraftprotect.init;
+
+import net.minecraft.item.Item;
+
+public class SCPItems {
+    public static Item.ToolMaterial toolMaterial1023;
+    public static Item scp1023ARC;
+}
