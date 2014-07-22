1753b196d1a937b4ff1bb24af7a0cd81474c1315
Marty Plummer <ntzrmtthihu777@gmail.com>
2014-07-22 07:16:55 -0500

    SCP-1023ARC

---
+++ src/main/java/securecraftprotect/common/item/ItemSCPKnife.java
@@ -0,0 +1,38 @@
+package securecraftprotect.common.item;
+
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.item.EnumRarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
+
+public class ItemSCPKnife extends ItemSword {
+    public int textureID;
+    public ItemSCPKnife(ToolMaterial material, int textureID) {
+        super(material);
+        this.textureID = textureID;
+    }
+
+    public boolean getIsRepairable(ItemStack itemToRepair,
+                                   ItemStack itemToRepairWith) {
+        return false;
+    }
+
+    public void registerIcons(IIconRegister iconRegister) {
+        switch (textureID) {
+            case 0:
+                itemIcon = iconRegister.registerIcon("scp:1023");
+                break;
+            default:
+                itemIcon = iconRegister.registerIcon("minecraft:slimeball");
+                break;
+        }
+    }
+
+    public boolean isFull3D() {
+        return true;
+    }
+
+    public EnumRarity getRarity(ItemStack stack) {
+        return EnumRarity.epic;
+    }
+}
