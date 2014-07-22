1cf3b93da0cdea79a76f9913fdb05363b2f845b6
Marty Plummer <ntzrmtthihu777@gmail.com>
2014-07-22 12:05:28 -0500

    renamed for consistancy

--- src/main/java/securecraftprotect/client/ClientProxy.java
+++ src/main/java/securecraftprotect/client/ClientProxy.java
@@ -1,8 +1,21 @@
 package securecraftprotect.client;
 
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import securecraftprotect.client.model.ModelSCP0173;
+import securecraftprotect.client.renderer.entity.RenderSCP0173;
 import securecraftprotect.common.CommonProxy;
+import securecraftprotect.common.entity.monster.EntitySCP0173;
 
 public class ClientProxy extends CommonProxy {
     public void init() {
+    }
+
+    private void render() {
+        RenderingRegistry.registerEntityRenderingHandler(EntitySCP0173.class,
+                new RenderSCP0173(new ModelSCP0173(), 0.5F));
+    }
+
+    public int addArmor(String armor) {
+        return RenderingRegistry.addNewArmourRendererPrefix(armor);
     }
 }
