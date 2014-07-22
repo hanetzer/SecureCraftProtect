d887f7127d3a5bc64c9b15b4fa63a2e8e258beb4
Marty Plummer <ntzrmtthihu777@gmail.com>
2014-07-22 07:16:36 -0500

    SCP-1023ARC

---
+++ src/main/java/securecraftprotect/core/SCPItem.java
@@ -0,0 +1,27 @@
+package securecraftprotect.core;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.item.Item;
+import net.minecraftforge.common.util.EnumHelper;
+import securecraftprotect.common.item.ItemSCPKnife;
+
+import static securecraftprotect.init.SCPItems.*;
+
+public class SCPItem {
+    public static void init() {
+        initMaterials();
+        regItems();
+    }
+
+    private static void initMaterials() {
+        toolMaterial1023 = EnumHelper.addToolMaterial("1023", 0, -1, 0.0F, 9998.0F, 0);
+    }
+    private static void regItems() {
+        scp1023ARC = registerItem(new ItemSCPKnife(toolMaterial1023, 0).setUnlocalizedName("scp:1023arc"));
+    }
+
+    public static Item registerItem(Item item) {
+        GameRegistry.registerItem(item, item.getUnlocalizedName().replace("item.scp:", ""));
+        return item;
+    }
+}
