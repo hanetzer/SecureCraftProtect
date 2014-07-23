eeee91c19db3c31892e725fc9c212f66e7afde87
Marty Plummer <ntzrmtthihu777@gmail.com>
2014-07-22 21:49:34 -0500

    fixed class-d armor

--- src/main/java/securecraftprotect/core/SCPItem.java
+++ src/main/java/securecraftprotect/core/SCPItem.java
@@ -3,10 +3,11 @@ package securecraftprotect.core;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.item.Item;
 import net.minecraftforge.common.util.EnumHelper;
+import securecraftprotect.SCP;
+import securecraftprotect.common.item.ItemArmorClassD;
 import securecraftprotect.common.item.ItemSCPKnife;
 
-import static securecraftprotect.init.SCPItems.scp1023ARC;
-import static securecraftprotect.init.SCPItems.toolMaterial1023;
+import static securecraftprotect.init.SCPItems.*;
 
 public class SCPItem {
     public static void init() {
@@ -16,10 +17,15 @@ public class SCPItem {
 
     private static void initMaterials() {
         toolMaterial1023 = EnumHelper.addToolMaterial("1023", 0, -1, 0.0F, 9998.0F, 0);
+        armorMaterialClassD = EnumHelper.addArmorMaterial("CLASSD", 29, new int[] {2, 7, 5, 3}, 9);
     }
 
     private static void regItems() {
-        scp1023ARC = registerItem(new ItemSCPKnife(toolMaterial1023, 0).setUnlocalizedName("scp:1023arc"));
+        scp1023ARC  = registerItem(new ItemSCPKnife(toolMaterial1023, 0).setUnlocalizedName("scp:1023arc"));
+        gasMask     = registerItem(new ItemArmorClassD(armorMaterialClassD, SCP.proxy.addArmor("CLASSD"), 0).setUnlocalizedName("scp:gas_mask"));
+        classDShirt = registerItem(new ItemArmorClassD(armorMaterialClassD, SCP.proxy.addArmor("CLASSD"), 1).setUnlocalizedName("scp:classd_shirt"));
+        classDPants = registerItem(new ItemArmorClassD(armorMaterialClassD, SCP.proxy.addArmor("CLASSD"), 2).setUnlocalizedName("scp:classd_pants"));
+        classDBoots = registerItem(new ItemArmorClassD(armorMaterialClassD, SCP.proxy.addArmor("CLASSD"), 3).setUnlocalizedName("scp:classd_boots"));
     }
 
     public static Item registerItem(Item item) {
--- src/main/java/securecraftprotect/init/SCPItems.java
+++ src/main/java/securecraftprotect/init/SCPItems.java
@@ -1,9 +1,15 @@
 package securecraftprotect.init;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemArmor;
 
 public class SCPItems {
     public static Item.ToolMaterial toolMaterial1023;
+    public static ItemArmor.ArmorMaterial armorMaterialClassD;
     public static Item scp1023ARC;
     public static Item gasMask;
+    public static Item classDShirt;
+    public static Item classDPants;
+    public static Item classDBoots;
+
 }
