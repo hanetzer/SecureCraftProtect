590c3163f999070793570b3c95a746e921076080
Marty Plummer <ntzrmtthihu777@gmail.com>
2014-07-23 14:55:58 -0500

    added block tab

--- src/main/java/securecraftprotect/SCP.java
+++ src/main/java/securecraftprotect/SCP.java
@@ -4,13 +4,16 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraft.creativetab.CreativeTabs;
 import securecraftprotect.common.CommonProxy;
+import securecraftprotect.common.creativetab.SCPTileTab;
 import securecraftprotect.common.handlers.SCPEventHandler;
 import securecraftprotect.core.SCPEntity;
 import securecraftprotect.core.SCPItem;
+import securecraftprotect.core.SCPTile;
 
 //import cpw.mods.fml.common.network.NetworkRegistry;
-
+@SuppressWarnings("unused")
 @Mod(modid = "scp", name = "SecureCraftProtect", version = "@VERSION@")
 public class SCP {
     @Mod.Instance("scp")
@@ -22,12 +25,15 @@ public class SCP {
             clientSide = "securecraftprotect.client.ClientProxy",
             serverSide = "securecraftprotect.common.CommonProxy")
     public static CommonProxy proxy;
+    public static CreativeTabs scpTile;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         //NetworkRegistry.INSTANCE.registerGuiHandler(this, proxy);
+        scpTile = new SCPTileTab(CreativeTabs.getNextID(), "scpTile");
         SCPEventHandler.init();
         SCPItem.init();
+        SCPTile.init();
         SCPEntity.init();
     }
 
---
+++ src/main/java/securecraftprotect/common/creativetab/SCPTileTab.java
@@ -0,0 +1,15 @@
+package securecraftprotect.common.creativetab;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import securecraftprotect.init.SCPTiles;
+
+public class SCPTileTab extends CreativeTabs {
+    public SCPTileTab(int position, String name) {
+        super(position, name);
+    }
+    @Override
+    public Item getTabIconItem() {
+        return Item.getItemFromBlock(SCPTiles.locker);
+    }
+}
