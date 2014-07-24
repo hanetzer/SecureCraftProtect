c7c0d3ff7365bd9d9f5715c0dc11aaed067b37ce
Marty Plummer <ntzrmtthihu777@gmail.com>
2014-07-24 06:31:47 -0500

    adjusted blink speed to 400

--- src/main/java/securecraftprotect/util/Globals.java
+++ src/main/java/securecraftprotect/util/Globals.java
@@ -5,7 +5,7 @@ public class Globals {
     public static int KEY_BLINKING = 0;
     public static int KEY_BLINK = 1;
     public static int KEY_REMOTE = 2;
-    public static int MAX_BLINK = 300;
+    public static int MAX_BLINK = 400;
     public static int BLINK = 30;
     public static int BLINKING = 31;
 }
