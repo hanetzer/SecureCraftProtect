f4e54f1fef52a1fbd91fe6355996a0f5bab173c8
Marty Plummer <ntzrmtthihu777@gmail.com>
2014-07-30 04:55:49 -0500

    refactored gui code

--- src/main/java/securecraftprotect/client/gui/inventory/GuiDocument.java
+++ src/main/java/securecraftprotect/client/gui/inventory/GuiDocument.java
@@ -8,24 +8,29 @@ import securecraftprotect.common.inventory.ContainerDocument;
 
 import static org.lwjgl.opengl.GL11.glColor4f;
 
-public class GuiDocument extends GuiContainer {
-    private ResourceLocation texture;
-    public GuiDocument(InventoryPlayer inventoryplayer,
-                       World world, int x, int y, int j) {
-        super(new ContainerDocument(inventoryplayer, world, x, y, j));
-        this.texture = new ResourceLocation("scp:textures/gui/document.png");
-    }
+public class GuiDocument extends GuiContainer
+{
+	private ResourceLocation texture;
 
-    protected void drawGuiContainerForegroundLayer() {
-        fontRendererObj.drawString("", 62, 6, 0x404040);
-        fontRendererObj.drawString("Inventory", 8, (ySize - 96) + 2, 0x404040);
-    }
+	public GuiDocument(InventoryPlayer inventory, World world, int x, int y,
+					   int z)
+	{
+		super(new ContainerDocument(inventory, world, x, y, z));
+		this.texture = new ResourceLocation("scp:textures/gui/document.png");
+	}
 
-    protected void drawGuiContainerBackgroundLayer(float f, int i, int j) {
-        glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        mc.getTextureManager().bindTexture(texture);
-        j = (width - xSize) / 2;
-        int k = (height - ySize) / 2;
-        drawTexturedModalRect(j, k, 0, 0, xSize, ySize);
-    }
+	protected void drawGuiContainerForegroundLayer()
+	{
+		fontRendererObj.drawString("", 62, 6, 0x404040);
+		fontRendererObj.drawString("Inventory", 8, (ySize - 96) + 2, 0x404040);
+	}
+
+	protected void drawGuiContainerBackgroundLayer(float f, int x, int y)
+	{
+		glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		mc.getTextureManager().bindTexture(texture);
+		y = (width - xSize) / 2;
+		int k = (height - ySize) / 2;
+		drawTexturedModalRect(y, k, 0, 0, xSize, ySize);
+	}
 }
