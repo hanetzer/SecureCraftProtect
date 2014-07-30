6ec202aaa3b0d3dd9d3ed142655ecc5f35d0d8fd
Marty Plummer <ntzrmtthihu777@gmail.com>
2014-07-30 05:00:34 -0500

    added acid bucket and keycard base

--- src/main/java/securecraftprotect/common/item/ItemBucket.java
+++ src/main/java/securecraftprotect/common/item/ItemBucket.java
@@ -29,89 +29,89 @@ public class ItemBucket extends Item
      * Called whenever this item is equipped and the right mouse button is
      * pressed. Args: itemStack, world, entityPlayer
      */
-    public ItemStack onItemRightClick(ItemStack stack, World par2World, EntityPlayer par3EntityPlayer)
+    public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player)
     {
         boolean flag = SCPTiles.acid == Blocks.air;
-        MovingObjectPosition movingobjectposition = this.getMovingObjectPositionFromPlayer(par2World, par3EntityPlayer, flag);
+        MovingObjectPosition pos = this.getMovingObjectPositionFromPlayer(world, player, flag);
         
-        if (movingobjectposition == null)
+        if (pos == null)
         {
             return stack;
         }
         else
         {
-            FillBucketEvent event = new FillBucketEvent(par3EntityPlayer, stack, par2World, movingobjectposition);
-            if (MinecraftForge.EVENT_BUS.post(event)) { return stack; }
+            FillBucketEvent event = new FillBucketEvent(player, stack, world, pos);
+            if (MinecraftForge.EVENT_BUS.post(event))
+			{
+				return stack;
+			}
             
             if (event.getResult() == Event.Result.ALLOW)
             {
-                if (par3EntityPlayer.capabilities.isCreativeMode) { return stack; }
+                if (player.capabilities.isCreativeMode) { return stack; }
                 
                 if (--stack.stackSize <= 0) { return event.result; }
                 
-                if (!par3EntityPlayer.inventory.addItemStackToInventory(event.result))
+                if (!player.inventory.addItemStackToInventory(event.result))
                 {
-                    par3EntityPlayer.dropPlayerItemWithRandomChoice(event.result, false);
+                    player.dropPlayerItemWithRandomChoice(event.result, false);
                 }
                 
                 return stack;
             }
-            if (movingobjectposition.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK)
+            if (pos.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK)
             {
-                int i = movingobjectposition.blockX;
-                int j = movingobjectposition.blockY;
-                int k = movingobjectposition.blockZ;
+                int x = pos.blockX;
+                int y = pos.blockY;
+                int z = pos.blockZ;
                 
-                if (!par2World.canMineBlock(par3EntityPlayer, i, j, k)) { return stack; }
+                if (!world.canMineBlock(player, x, y, z)) { return stack; }
                 
                 if (flag)
                 {
-                    if (!par3EntityPlayer.canPlayerEdit(i, j, k, movingobjectposition.sideHit, stack)) { return stack; }
+                    if (!player.canPlayerEdit(x, y, z, pos.sideHit, stack)) { return stack; }
                     
-                    Material material = par2World.getBlock(i, j, k).getMaterial();
-                    int l = par2World.getBlockMetadata(i, j, k);
-                    par2World.setBlockToAir(i, j, k);
-                    return this.func_150910_a(stack, par3EntityPlayer, SCPItems.bucket);
+                    Material material = world.getBlock(x, y, z).getMaterial();
+                    int l = world.getBlockMetadata(x, y, z);
+                    world.setBlockToAir(x, y, z);
+                    return this.func_150910_a(stack, player, SCPItems.bucket);
                 }
                 else
                 {
-                    if (SCPTiles.acid == Blocks.air) { return new ItemStack(Items.bucket); }
-                    
-                    if (movingobjectposition.sideHit == 0)
-                    {
-                        --j;
-                    }
-                    
-                    if (movingobjectposition.sideHit == 1)
-                    {
-                        ++j;
-                    }
-                    
-                    if (movingobjectposition.sideHit == 2)
-                    {
-                        --k;
-                    }
-                    
-                    if (movingobjectposition.sideHit == 3)
-                    {
-                        ++k;
-                    }
-                    
-                    if (movingobjectposition.sideHit == 4)
-                    {
-                        --i;
-                    }
-                    
-                    if (movingobjectposition.sideHit == 5)
-                    {
-                        ++i;
-                    }
-                    
-                    if (!par3EntityPlayer.canPlayerEdit(i, j, k, movingobjectposition.sideHit, stack)) { return stack; }
-                    
+                    if (SCPTiles.acid == Blocks.air) {
+						return new ItemStack(Items.bucket);
+					}
+                    switch (pos.sideHit) {
+						case 0:
+							--y;
+							break;
+						case 1:
+							++y;
+							break;
+						case 2:
+							--z;
+							break;
+						case 3:
+							++z;
+							break;
+						case 4:
+							--x;
+							break;
+						case 5:
+							++x;
+							break;
+					}
+
+                    if (!player.canPlayerEdit(x, y, z, pos.sideHit, stack)) {
+						return stack;
+					}
                     try
                     {
-                        if (this.tryPlaceContainedLiquid(par2World, i, j, k, stack) && !par3EntityPlayer.capabilities.isCreativeMode) { return new ItemStack(Items.bucket); }
+						if (this.tryPlaceContainedLiquid(world, x, y, z, stack)) {
+							if (!player.capabilities.isCreativeMode) {
+								return new ItemStack(Items.bucket);
+							}
+						}
                     }
                     catch (IOException e)
                     {
@@ -125,24 +125,24 @@ public class ItemBucket extends Item
         }
     }
     
-    private ItemStack func_150910_a(ItemStack p_150910_1_, EntityPlayer p_150910_2_, Item p_150910_3_)
+    private ItemStack func_150910_a(ItemStack stack, EntityPlayer player, Item item)
     {
-        if (p_150910_2_.capabilities.isCreativeMode)
+        if (player.capabilities.isCreativeMode)
         {
-            return p_150910_1_;
+            return stack;
         }
-        else if (--p_150910_1_.stackSize <= 0)
+        else if (--stack.stackSize <= 0)
         {
-            return new ItemStack(p_150910_3_);
+            return new ItemStack(item);
         }
         else
         {
-            if (!p_150910_2_.inventory.addItemStackToInventory(new ItemStack(p_150910_3_)))
+            if (!player.inventory.addItemStackToInventory(new ItemStack(item)))
             {
-                p_150910_2_.dropPlayerItemWithRandomChoice(new ItemStack(p_150910_3_, 1, 0), false);
+                player.dropPlayerItemWithRandomChoice(new ItemStack(item, 1, 0), false);
             }
             
-            return p_150910_1_;
+            return stack;
         }
     }
     
