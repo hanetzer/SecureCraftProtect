4f90bcfc194659e150a28ccbf4b127d8ae42aefd
Marty Plummer <ntzrmtthihu777@gmail.com>
2014-08-02 23:37:15 -0500

    removed current code from old branch

--- src/main/java/securecraftprotect/common/tile/ITileFurnature.java
+++
@@ -1,9 +0,0 @@
-package securecraftprotect.common.tile;
-
-public interface ITileFurnature
-{
-	String[][] types = {
-			{"oak", "spruce", "birch", "acacia", "big_oak"},
-			{"stone", "scp:granite", "scp:marble"}
-	};
-}
