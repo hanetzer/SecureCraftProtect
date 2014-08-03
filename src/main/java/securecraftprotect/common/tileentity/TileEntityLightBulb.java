7bb36b2d875edc8656b55b7a65ccd3d81656f119
OnyxDarkKnight <sor1n.iliutza16@gmail.com>
2014-08-03 22:00:20 +0300

    stuff

--- src/main/java/securecraftprotect/common/tileentity/TileEntityLightBulb.java
+++ src/main/java/securecraftprotect/common/tileentity/TileEntityLightBulb.java
@@ -1,5 +1,6 @@
 package securecraftprotect.common.tileentity;
 
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.EnumSkyBlock;
 
@@ -12,10 +13,18 @@ public class TileEntityLightBulb extends TileEntity
     
     public void updateEntity()
     {
-        this.worldObj.setLightValue(EnumSkyBlock.Block, this.xCoord, this.yCoord, this.zCoord, 6);
-        this.markDirty();
+        this.worldObj.setLightValue(EnumSkyBlock.Block, this.xCoord, this.yCoord, this.zCoord, 12);
+        this.updateContainingBlockInfo();
 //        this.worldObj.updateLightByType(EnumSkyBlock.Block, this.xCoord, this.yCoord, this.zCoord);
         //this.worldObj.markTileEntityChunkModified(this.xCoord, this.yCoord, this.zCoord, this);
+    }
+
+    public void readFromNBT(NBTTagCompound tag)
+    {
+    }
+
+    public void writeToNBT(NBTTagCompound tag)
+    {
     }
     
 }
